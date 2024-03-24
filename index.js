58f916de810b2f01a3f12047aaf9b01bc5288de1
BrianOnchwari <onchwaribrian6@gmail.com>
2024-03-24 13:55:20 +0300

    Functions Continued

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// Function to define Saturday activities
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Function to define Monday work
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+// Function to create a wrapper function for adjectives
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+// Exporting functions
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+};
